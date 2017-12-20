d4e88b1adae5bde41d8659edf42ecc2ae1d50bf3
Gabriel Caruana <gabb1995@gmail.com>
2017-12-20 16:13:01 +0100

    #1 - Added 2 new fields

--- randomNumber.js
+++ randomNumber.js
@@ -14,12 +14,44 @@ module.exports.templateTags = [{
             description: 'Max',
             type: 'number',
             defaultValue: 10000
+        },
+        {
+            help: 'Adds leading 0s according to length of number',
+            displayName: 'Length',
+            description: 'Length',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            help: 'Adds decimal point to random number',
+            displayName: 'Decimal Point',
+            description: 'Decimal Point',
+            type: 'number',
+            defaultValue: 0
         }
     ],
-    async run (context, min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
+    async run (context, min, max, length = 0, decimal = 0) {
+        if (max.toString().length > length && length !== 0) {
+            throw new Error("Length has to be bigger than the max number's length");
+        }
 
-        return Math.floor(Math.random() * (max - min)) + min;
+        min = Math.ceil(min * Math.pow(10, decimal));
+        max = Math.floor(max * Math.pow(10, decimal));
+
+        randomNumber = (Math.floor(Math.random() * (max - min)) + min).toString();
+
+        if (decimal > 0) {
+            randomNumber = randomNumber.slice(0, (decimal * -1)) + '.' + randomNumber.slice(decimal * -1);
+            while(randomNumber.slice(0, ((decimal + 1) * -1)).length < length) {
+                randomNumber = "0" + randomNumber;
+            }
+        } else {
+            while (randomNumber.length < length) {
+                randomNumber = "0" + randomNumber;
+            }
+        }
+
+
+        return randomNumber;
     }
 }];
